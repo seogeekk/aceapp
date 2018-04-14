300a8f48827d87185c0a0ba4ec28aa9d3054727b
seogeekk <seogeekk@hotmail.com>
2018-04-14 15:36:07 +1000

    Update installation scripts

--- scripts/replacehost.pl
+++ scripts/replacehost.pl
@@ -5,11 +5,12 @@ use strict;
 my $external_ip = $ARGV[0];
 
 if (!$external_ip) {
-	printf "Please provide an external ip";
+	printf "Please provide an external ip \n";
 	exit 1;
 }
 
 my $replace = quotemeta($external_ip);
 
-`perl -i -ple "s/localhost\:3000/$replace\:8080/g" /var/www/html/components/services/*.js`
-`perl -i -ple "s/localhost\:80/$replace\:80/g" /usr/pkg/aceapi/routes/handlers/*.js`
+
+`perl -i -ple "s/http\:/https\:/g" /var/www/html/components/services/*.js`;
+`perl -i -ple "s/localhost\:3000/$replace/g" /var/www/html/components/services/*.js`;
